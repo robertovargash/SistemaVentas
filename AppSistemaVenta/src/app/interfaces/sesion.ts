a969f14b65f13000963a967af6d91ffc1c1d0e99
Roberto vargas <robertovargashdez@gmail.com>
2023-07-13 08:16:57 -0400

    AngularApp, con interfaces

---
+++ AppSistemaVenta/src/app/interfaces/sesion.ts
@@ -0,0 +1,6 @@
+export interface Sesion {
+    idUusuario:number,
+    nombreCompleto:string,
+    correo: string,
+    rolDescripcion: string
+}
---
+++ AppSistemaVenta/src/app/interfaces/usuario.ts
@@ -0,0 +1,9 @@
+export interface Usuario {
+    idUsuario: number,
+    nombreCompleto: string,
+    correo: string,
+    idRol: number,
+    rolDesripcion: string,
+    clave: string,
+    esActivo: number
+}
