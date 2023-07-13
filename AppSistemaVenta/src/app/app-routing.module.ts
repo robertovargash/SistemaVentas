888a867a63901dc52fd7ee9322e3c457bb317fdb
Roberto vargas <robertovargashdez@gmail.com>
2023-07-13 18:32:55 -0400

    Paginas y componentes

--- AppSistemaVenta/src/app/app-routing.module.ts
+++ AppSistemaVenta/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { LayoutRoutingModule } from './components/layout/layout-routing.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:LoginComponent, pathMatch:"full"},
+  {path:'login', component:LoginComponent, pathMatch:"full"},
+  {path:'pages', loadChildren: () => import("./components/layout/layout.module").then(m => m.LayoutModule)},//obtiene todas las páginas
+  {path:'**', redirectTo:'login',pathMatch:"full"},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ AppSistemaVenta/src/app/components/layout/layout.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LayoutRoutingModule } from './layout-routing.module';
+import { DashBoardComponent } from './pages/dash-board/dash-board.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { ProductoComponent } from './pages/producto/producto.component';
+import { VentaComponent } from './pages/venta/venta.component';
+import { HistorialVentaComponent } from './pages/historial-venta/historial-venta.component';
+import { ReporteComponent } from './pages/reporte/reporte.component';
+import { SharedModule } from 'src/app/reutilizable/shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    DashBoardComponent,
+    UsuarioComponent,
+    ProductoComponent,
+    VentaComponent,
+    HistorialVentaComponent,
+    ReporteComponent    
+  ],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule,
+    SharedModule
+  ]
+})
+export class LayoutModule { }
